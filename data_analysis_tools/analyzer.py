91eb0d886965f63b2456951a07d578f54121ff30
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2024-02-13 13:21:50 -0700

    winrate by race

--- data_analysis_tools/analyzer.py
+++ data_analysis_tools/analyzer.py
@@ -7,70 +7,79 @@ import os
 import sc2reader
 from enum import Enum
 from database_tools.sc2_database import retrieve_table_data
+import random
 
-def winrate_by_race():
-    games = retrieve_table_data('games')
+class Stats:
+    def __init__(self, game_data) -> None:
+        self.game_data = game_data
 
-    zerg_wins_vs_protoss = 0
-    zerg_wins_vs_terran = 0
-    terran_wins_vs_protoss = 0
+        self.tvp_wins = 0
+        self.tvz_wins = 0
+        self.zvp_wins = 0
 
-    zvp_total = 0
-    zvt_total = 0
-    tvp_total = 0
+        self.tvp_total = 0
+        self.tvz_total = 0
+        self.zvp_total = 0
 
-    for game in games:
-        if 'Zerg' in game and 'Protoss' in game:
-            zvp_total += 1
-            if game[6] == game[1]:
-                if game[2] == 'Zerg':
-                    zerg_wins_vs_protoss += 1
-            elif game[6] == game[3]:
-                if game[4] == 'Zerg':
-                    zerg_wins_vs_protoss += 1
-        elif 'Zerg' in game and 'Terran' in game:
-            zvt_total += 1
-            if game[6] == game[1]:
-                if game[2] == 'Zerg':
-                    zerg_wins_vs_terran += 1
-            elif game[6] == game[3]:
-                if game[4] == 'Zerg':
-                    zerg_wins_vs_terran += 1
-        elif 'Teran' in game and 'Protoss' in game:
-            tvp_total += 1
-            if game[6] == game[1]:
-                if game[2] == 'Terran':
-                    terran_wins_vs_protoss += 1
-            elif game[6] == game[3]:
-                if game[4] == 'Terran':
-                    terran_wins_vs_protoss += 1
-    print(zerg_wins_vs_protoss)
-    print(zerg_wins_vs_terran)
-    print(terran_wins_vs_protoss)
-    print(zvp_total)
-    print(zvt_total)
-    print(tvp_total)
+    def check_races_in_game(self, races: list, winner):
+        set_of_races = {races[0], races[1]}
+        tvp = {"Terran", "Protoss"}
+        tvz = {"Terran", "Zerg"}
+        zvp = {"Zerg", "Protoss"}
+        winner_race = winner[0]
 
+        if tvp.issubset(set_of_races):
+            self.tvp_total += 1
+            if winner_race == "Terran":
+                self.tvp_wins += 1
+        elif tvz.issubset(set_of_races):
+            self.tvz_total += 1
+            if winner_race == "Terran":
+                self.tvz_wins += 1
+        elif zvp.issubset(set_of_races):
+            if winner_race == "Zerg":
+                self.zvp_wins += 1
+            self.zvp_total += 1
+        else:
+            print("ERROR in check_races_in_game")
 
-
-
+    def wr_race(self):
+        for game in self.game_data:
+            races = game[0]
+            winner = game[1]
+            self.check_races_in_game(races, winner)
+        tvp_wr = round((self.tvp_wins / self.tvp_total) * 100, 2)
+        tvz_wr = round((self.tvz_wins / self.tvz_total) * 100, 2)
+        zvp_wr = round((self.zvp_wins / self.zvp_total) * 100, 2)
+        return (tvp_wr, tvz_wr, zvp_wr)
 
 
 def main():
     """Main entry point"""
-    # print("\nPRINTING DATA IN 'GAMES' TABLE...")
-    # row = retrieve_table_data('games', 1) # Get just a single row of data
-    # table_data = retrieve_table_data('games') # Retrieve all the data from 'games' table
-    # for row in table_data:
-    #     print(row)
-    #     for data in row:
-    #         print(data)
-    #     print()
+    # Generate random races and winners for testing
+    def generate_dummy_game_data(num_games):
+        races = ["Terran", "Protoss", "Zerg"]
+        game_data = []
+        for _ in range(num_games):
+            race1 = random.choice(races)
+            race2 = random.choice([race for race in races if race != race1])
+            winner = random.choice([race1, race2])
+            game_data.append(([race1, race2], [winner]))
+        return game_data
 
-    test = (1,2,3)
-    print(test[0])
-    winrate_by_race()
+    # Generate 1000 dummy games for testing
+    game_data = generate_dummy_game_data(1000)
+    # print(game_data)
 
+    # Instantiate Stats object with the generated dummy data
+    stats = Stats(game_data)
+
+    # Calculate and print win rates
+    print(stats.tvp_total)
+    print("Win rates:")
+    print(f"Terran vs Protoss:{stats.wr_race()[0]}%")
+    print(f"Terran vs Zerg:{stats.wr_race()[1]}%")
+    print(f"Zerg vs Protoss: {stats.wr_race()[2]}%")
 # Interpret this module
 if __name__ == "__main__":
     main()
