b906e65f40c49cfb2fa29a5cfe05a2314b2c4c5b
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2024-02-12 21:50:20 -0700

    demo

--- data_analysis_tools/analyzer.py
+++ data_analysis_tools/analyzer.py
@@ -10,7 +10,15 @@ from database_tools.sc2_database import insert_into_db, retrieve_table_data
 
 def main():
     """Main entry point"""
-    
+
+    print("\nPRINTING DATA IN 'GAMES' TABLE...")
+    row = retrieve_table_data('games', 1) # Get just a single row of data
+    table_data = retrieve_table_data('games') # Retrieve all the data from 'games' table
+    for row in table_data:
+        print(row)
+    print('\n')
+    for data in row:
+        print(data)    
 
 # Interpret this module
 if __name__ == "__main__":
