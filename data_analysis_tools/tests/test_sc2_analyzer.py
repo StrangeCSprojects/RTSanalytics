7c4d1d677e4063a864e2f64766d16cdee0132bd3
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2024-03-25 14:56:46 -0600

    Update test_sc2_analyzer.py

--- data_analysis_tools/tests/test_sc2_analyzer.py
+++ data_analysis_tools/tests/test_sc2_analyzer.py
@@ -1,6 +1,7 @@
 import pytest
 from data_analysis_tools.sc2.sc2_analyzer import SC2Analyzer
 import random
+from typing import Callable
 
 
 def create_player_name():
@@ -82,7 +83,7 @@ def create_aggr_commands():
 ]
     return commands_aggr_terran
 
-def create_std_commmands():
+def create_std_commands():
     commands_std_terran = [
             # Economy resources: 2200
             (("UnitBornEvent", "SCV"), 1),
@@ -169,62 +170,85 @@ def create_id():
     player_id = random.randint(0, 9999999)
     return player_id
 
-def generate_game_records(num_records):
-    for _ in range(num_records):
-        # Player one table
-        player_one_id = create_id()
-        player_one_name = create_player_name()
+def create_game_record(build_type_one: Callable, build_type_two: Callable, player_one_win: bool):
+    # Player one table
+    player_one_id = create_id()
+    player_one_name = create_player_name()
 
-        # Player two table
-        player_two_id = create_id()
-        player_two_name = create_player_name()
+    # Player two table
+    player_two_id = create_id()
+    player_two_name = create_player_name()
 
-        # Game table
-        game_id = create_id()
-        game_mode = "1v1"
-        game_map = "Solaris LE"
+    # Game table
+    game_id = create_id()
+    game_mode = "1v1"
+    game_map = "Solaris LE"
 
-        # Player one play table
-        game_one_id = game_id
-        player_one_id = player_one_id
-        race_one = "Terran"
-        winner_one = True
-        commands_one = None
+    # Player one play table
+    game_one_id = game_id
+    player_one_id = player_one_id
+    race_one = "Terran"
+    winner_one = player_one_win
+    commands_one = build_type_one()
 
-        # Player two play table
-        game_two_id = game_id
-        player_two_id = player_two_id
-        race_two = "Terran"
-        winner_two = False
-        commands_two = None
+    # Player two play table
+    game_two_id = game_id
+    player_two_id = player_two_id
+    race_two = "Terran"
+    winner_two = not winner_one
+    commands_two = build_type_two()
 
-        player_one_data = (player_one_id, player_one_name)
-        player_two_data = (player_two_id, player_two_name)
-        game_data = (game_id, game_mode, game_map)
-        player_one_play_data = (
-            game_one_id,
-            player_one_id,
-            race_one,
-            winner_one,
-            commands_one,
-        )
-        player_two_play_data = (
-            game_two_id,
-            player_two_id,
-            race_two,
-            winner_two,
-            commands_two,
-        )
+    player_one_data = (player_one_id, player_one_name)
+    player_two_data = (player_two_id, player_two_name)
 
-        record = (
-            player_one_data,
-            player_two_data,
-            game_data,
-            player_one_play_data,
-            player_two_play_data,
-        )
 
-        yield record
+    game_data = (game_id, game_mode, game_map)
+    play_one_data = (
+        game_one_id,
+        player_one_id,
+        race_one,
+        winner_one,
+        commands_one,
+    )
+    play_two_data = (
+        game_two_id,
+        player_two_id,
+        race_two,
+        winner_two,
+        commands_two,
+    )
+
+    record = (
+        player_one_data,
+        player_two_data,
+        game_data,
+        play_one_data,
+        play_two_data,
+    )
+
+    return record
+
+def generate_std_vs_aggr_60():
+    for i in range(10):
+        if i < 6:
+            yield create_game_record(create_std_commands, create_aggr_commands, True)
+        else:
+            yield create_game_record(create_std_commands, create_aggr_commands, False)
+
+def generate_aggr_vs_eco_80():
+    for i in range(10):
+        if i < 8:
+            yield create_game_record(create_aggr_commands, create_eco_commands, True)
+        else:
+            yield create_game_record(create_aggr_commands, create_eco_commands, False)
+
+def generate_eco_vs_std_70():
+    for i in range(10):
+        if i < 7:
+            yield create_game_record(create_eco_commands, create_std_commands, True)
+        else:
+            yield create_game_record(create_eco_commands, create_std_commands, False)
+
 
 
 def test_winrate_build():
@@ -233,3 +257,11 @@ def test_winrate_build():
 
 def test_winrate_race():
     pass
+
+def main():
+    for record in generate_std_vs_aggr_60():
+        print(record)
+        print()
+
+if __name__ == "__main__":
+    main()
