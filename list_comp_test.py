e41b18a11c17b8cd4909ca10dfe60f9bb188761a
Clstrange <122171025+StrangeCSprojects@users.noreply.github.com>
2024-04-11 02:43:48 -0600

    Delete list_comp_test.py

--- list_comp_test.py
+++
@@ -1,6 +0,0 @@
-nested_list = [[["unit type", "unit name"], 10], 1]
-
-# Updated list comprehension to handle the deeper nested structure
-result_commands = [((tuple(inner_list[0][0]), inner_list[0][1]), inner_list[1]) for inner_list in [nested_list]]
-
-print(result_commands)
