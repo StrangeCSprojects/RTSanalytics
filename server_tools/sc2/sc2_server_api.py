f4e8410cf82ae486e36ccc07af9da28f9e259405
Jaden <jadenlalbrecht@gmail.com>
2024-04-18 14:12:02 -0600

    Some changes to init function.

--- server_tools/sc2/sc2_server_api.py
+++ server_tools/sc2/sc2_server_api.py
@@ -1,21 +1,28 @@
-from data_analysis_tools.general.analyzer import Analyzer
-from database_tools.general.data_retriever import DataRetriever
+from data_analysis_tools.sc2.sc2_analyzer import SC2Analyzer
+from database_tools.sc2.sc2_build_order_data_retriever import SC2BuildOrderDataRetriever
+from database_tools.sc2.sc2_build_order_database import SC2BuildOrderDB
+from database_tools.sc2.sc2_replay_data_retriever import SC2ReplayDataRetriever
+from database_tools.sc2.sc2_replay_database import SC2ReplayDB
 from server_tools.interfaces.GetBuildOrders import GetBuildOrders
 from server_tools.interfaces.DisplayOverlay import DisplayOverlay
 from server_tools.interfaces.GetWinratesRace import GetWinratesRace
-from data_analysis_tools.general.build_order.build_order_overlay import (
-    BuildOrderOverlay,
+from data_analysis_tools.sc2.sc2_build_order.sc2_build_order_overlay import (
+    SC2BuildOrderOverlay,
 )
 
-class SC2ServerAPI(GetBuildOrders, DisplayOverlay, GetWinratesRace):
-    # Declare the three class variables
-    overlay: BuildOrderOverlay
-    data_retriever: DataRetriever
-    analyzer: Analyzer
 
-    def __init__(cls):
+class SC2ServerAPI(GetBuildOrders, DisplayOverlay, GetWinratesRace):
+    """
+    The server interface for interacting with SC2 RTS analytics backend functionality
+    """
+
+    def __init__(self):
         # Initialize your class attributes here if needed
-        pass
+        # Finish this later
+        replay_data_retreiver = SC2ReplayDataRetriever(SC2ReplayDB)
+        self.analyzer = SC2Analyzer()
+        self.data_retriever = SC2BuildOrderDataRetriever(SC2BuildOrderDB)
+        self.overlay = SC2BuildOrderOverlay()
 
     def get_build_orders(cls):
         # Implement the functionality to get build orders
