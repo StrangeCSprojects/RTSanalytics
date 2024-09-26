2df7d94301d618a60fe7cec7450ebed0717c27b8
StrangeCSprojects <122171025+StrangeCSprojects@users.noreply.github.com>
2024-09-26 15:51:14 -0600

    finished (#61)

---
+++ data_analysis_tools/general/reports/major_battle_units_lost.py
@@ -0,0 +1,43 @@
+from data_analysis_tools.general.reports.report import (
+    Report,
+)  # Base class for creating reports.
+from database_tools.general.data_retriever import (
+    DataRetriever,
+)  # Class responsible for retrieving data from the database.
+from database_tools.general.general_database import (
+    GeneralDB,
+)  # General database interaction class.
+
+
+class MajorBattleUnitsLost(Report):
+    """
+    A class that generates reports on the location of major battles based on unit death data.
+    Inherits from the Report class.
+    """
+
+    def __init__(
+        self,
+        data_retriever: DataRetriever,
+        time_of_battle: tuple[int, int],
+        location: tuple[int, int],
+    ) -> None:
+        """
+        Initialize the MajorBattleUnitsLost class with a data retriever, time of battle, and location.
+
+        :param data_retriever: An instance of DataRetriever for accessing unit death location data.
+        :param time_of_battle: A tuple (start_time, end_time) representing the time range of the battle (in-game time).
+        :param location: A tuple (center_x, center_y, radius) representing the center and radius of the circle
+                         that defines the battle's location on the map.
+        """
+        super().__init__(data_retriever)
+        self.time = time_of_battle
+        self.loc = location
+        self.data_retriever = data_retriever
+
+    def configure_report(self) -> list:
+        """
+        Configure the report by retrieving unit death data based on the time and location of the battle.
+
+        :return: A list of unit data retrieved based on the battle's location and time.
+        """
+        return self.data_retriever.get_units_lost_by_location_and_time()
